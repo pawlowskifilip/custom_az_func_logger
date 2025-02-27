c5cfa670665be13a8df72d78b08deb7c722434d1
Filip Pawlowski <pawlows.filip@gmail.com>
2025-02-27 17:34:50 +0100

    Second initial commit

---
+++ function_app.py
@@ -0,0 +1,20 @@
+import datetime
+import json
+import logging
+
+import azure.functions as func
+
+app = func.FunctionApp()
+
+
+@app.route(route="AzLogger", auth_level=func.AuthLevel.ANONYMOUS)
+def AzLogger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    number_list = [x for x in range(10)]
+    logging.info(number_list)
+
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        status_code=200,
+    )
